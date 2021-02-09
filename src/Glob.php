205ff17c4a7f10b565920c36780e6aecb6bdc171
wuding <netjoin@gmail.com>
2021-02-09 15:39:49 +0800

    添加检测语言、导入语言方法

--- src/Glob.php
+++ src/Glob.php
@@ -8,9 +8,17 @@ class Glob
     依赖
     */
     // 导入的配置数组
+    // 计划：通过函数导入
     public static $conf = array();
     // 内存缓存服务器
     public static $mem = null;
+
+    /*
+    变量
+    */
+    // 对应方法结果
+    public static $lang = null;
+    public static $lng = null;
 
     /*
     配置
@@ -43,5 +51,44 @@ class Glob
             $conf = self::conf($value);
         }
         return $conf;
+    }
+
+    /*
+    语言
+    */
+
+    // 客户端首选语言
+    public static function lang($locale = null)
+    {
+        $lang = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? null;
+        $LANG = preg_split("/,/", $lang);
+        $lng = array_shift($LANG);
+        // 计划：中文分简繁体
+        $pos = strpos($lng, '-');
+        if (false !== $pos) {
+            $lng = substr($lng, 0, $pos);
+        }
+        if (!$lng) {
+            return self::$lang = $locale;
+        }
+        return self::$lang = strtolower($lng);
+    }
+
+    // 导入语言配置
+    public static function lng($locale = null, $directory = null, $available_languages = null, $check_language = null)
+    {
+        $lang = false !== $check_language ? self::lang($locale) : $locale;
+        // 是否可用语言
+        if ($available_languages && !in_array($lang, $available_languages)) {
+            return $GLOBALS['_LANG'] = array();
+        }
+        // 包含文件
+        $_LANG = @include $directory ."/$lang.php";
+        self::$lng = $lang;
+        if (!is_array($_LANG)) {
+            $_LANG = array();
+            self::$lng = false;
+        }
+        return $GLOBALS['_LANG'] = $_LANG;
     }
 }
