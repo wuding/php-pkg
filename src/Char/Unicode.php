cf5675fc22a2581ea3f7bda9df15d37164fa4f32
wuding <netjoin@gmail.com>
2022-05-14 20:20:38 +0800

    新增字符编码 Unicode 类

---
+++ src/Char/Unicode.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Pkg\Char;
+
+class Unicode
+{
+    const VERSION = '22.5.14';
+
+    public function __construct()
+    {
+
+    }
+
+    public static function encode()
+    {
+
+    }
+
+    public static function decode()
+    {
+
+    }
+}
